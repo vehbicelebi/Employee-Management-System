1aa02e941817fe43f44ddfb6dff7b3a05a32ba80
vehbicelebi <vehbi.celebi2@gmail.com>
2024-03-10 12:51:10 +0100

    mostly finished

---
+++ src/main/java/com/example/crudwebapp/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.crudwebapp.repository;
+
+import com.example.crudwebapp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/main/java/com/example/crudwebapp/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.crudwebapp.service;
+
+import com.example.crudwebapp.model.Employee;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
